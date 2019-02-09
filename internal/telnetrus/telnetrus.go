c675d64ca9931be119a9d9180d7574bd6436f3b5
Petr Karmashev <smonkl@bk.ru>
2019-02-09 21:22:31 +0300

    Encoding and decoding in Windows1251

--- internal/telnetrus/telnetrus.go
+++ internal/telnetrus/telnetrus.go
@@ -1,7 +1,58 @@
 package telnetrus
 
-func Process(bytes []byte) (result string) {
-	result = string(bytes)
+import (
+	"errors"
+	"golang.org/x/text/encoding/charmap"
+	"strings"
+)
+
+const (
+	Windows1251 = "1"
+	KOI8        = "2"
+)
+
+func ToUTF8(encodingName string, bytes []byte) (result string, err error) {
+	characterMap, err := GetCharacterMap(encodingName)
+
+	if err != nil {
+		return
+	}
+
+	result, _ = characterMap.NewDecoder().String(string(bytes))
+
+	return
+}
+
+func FromUTF8(encodingName string, string string) (resultBytes []byte, err error) {
+	characterMap, err := GetCharacterMap(encodingName)
+
+	if err != nil {
+		return
+	}
+
+	// doubling "я" letter for CP1251 encoding
+	// @see http://citforum.ru/nets/semenov/4/45/tlnt_453.shtml
+	if encodingName == Windows1251 {
+		string = strings.Replace(string, "я", "яя", -1)
+	}
+
+	result, _ := characterMap.NewEncoder().String(string)
+	resultBytes = []byte(result)
+
+	return
+}
+
+func GetCharacterMap(characterMapName string) (characterMap *charmap.Charmap, err error) {
+	characterMaps := map[string]*charmap.Charmap{
+		Windows1251: charmap.Windows1251,
+		KOI8:        charmap.KOI8R,
+	}
+
+	if charMapName, ok := characterMaps[characterMapName]; ok {
+		characterMap = charMapName
+	} else {
+		err = errors.New("encoding is not supported: " + characterMapName)
+	}
 
 	return
 }
--- internal/telnetrus/telnetrus_test.go
+++ internal/telnetrus/telnetrus_test.go
@@ -8,12 +8,30 @@ import (
 	"github.com/stretchr/testify/suite"
 )
 
-var dataset = []struct {
-	input  []byte
-	output string
+var encodedDataset = []struct {
+	encoding string
+	input    []byte
+	output   string
 }{
-	{[]byte{48, 49}, "01"},
-	{[]byte{50, 51, 52}, "234"},
+	{Windows1251, []byte{0xff}, "я"},
+	{Windows1251, []byte{0xff, 0xff}, "яя"},
+	{Windows1251, []byte{0xff, 0xff, 0xff}, "яяя"},
+	{KOI8, []byte{0xd1}, "я"},
+	{KOI8, []byte{0xd1, 0xd1}, "яя"},
+	{KOI8, []byte{0xd1, 0xd1, 0xd1}, "яяя"},
+}
+
+var utfDataset = []struct {
+	encoding string
+	input    string
+	output   []byte
+}{
+	{Windows1251, "я", []byte{0xff, 0xff}},
+	{Windows1251, "яя", []byte{0xff, 0xff, 0xff, 0xff}},
+	{Windows1251, "яяя", []byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff}},
+	{KOI8, "я", []byte{0xd1}},
+	{KOI8, "яя", []byte{0xd1, 0xd1}},
+	{KOI8, "яяя", []byte{0xd1, 0xd1, 0xd1}},
 }
 
 func TestTelnetrus(test *testing.T) {
@@ -24,9 +42,17 @@ type telnetrusTest struct {
 	suite.Suite
 }
 
-func (suite *telnetrusTest) Test_Process_bytesWithYaLetter_correctString() {
-	for id, dataset := range dataset {
-		result := Process(dataset.input)
+func (suite *telnetrusTest) Test_ToUTF8_EncodingAndEncodedBytes_correctEncodedString() {
+	for id, dataset := range encodedDataset {
+		result, _ := ToUTF8(dataset.encoding, dataset.input)
+
+		assert.Equal(suite.T(), dataset.output, result, fmt.Sprintf("Dataset #%d", id), dataset.input, dataset.output)
+	}
+}
+
+func (suite *telnetrusTest) Test_FromUTF8_EncodingAndUTF8String_correctUTF8String() {
+	for id, dataset := range utfDataset {
+		result, _ := FromUTF8(dataset.encoding, dataset.input)
 
 		assert.Equal(suite.T(), dataset.output, result, fmt.Sprintf("Dataset #%d", id), dataset.input, dataset.output)
 	}
